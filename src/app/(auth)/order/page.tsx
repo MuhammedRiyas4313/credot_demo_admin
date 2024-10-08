a8b365375f1a19e2c6fad7becae1e063bdd37d71
Muhammed-Riyas-ebslon <148744494+Muhammed-Riyas-ebslon@users.noreply.github.com>
2024-10-08 10:07:57 +0530

    user update

--- src/app/(auth)/order/page.tsx
+++ src/app/(auth)/order/page.tsx
@@ -13,22 +13,13 @@ const Page = () => {
 
   return (
     <div style={{ backgroundColor: "#f0f0f2", padding: "20px", minHeight: "100vh" }}>
-      <SetHeaderName name="Brand" />
+      <SetHeaderName name="Oder" />
       <Container sx={{ mt: 2 }}>
-        <Grid sx={{ mb: 2 }}>
-          <Button
-            variant="contained"
-            sx={{ bgcolor: "#6d3481", ":hover": { bgcolor: "#823f99" } }}
-            onClick={() => setOpen(true)}
-          >
-            Add <AddIcon sx={{ ml: 1 }} />
-          </Button>
-        </Grid>
         <Grid>
           <ViewOrder setId={setId} setOpen={setOpen} />
         </Grid>
       </Container>
-      <CustomDialog title={`${id ? "update" : "Add new"} brand`} open={open} setOpen={setOpen} setId={setId}>
+      <CustomDialog title={`update Order`} open={open} setOpen={setOpen} setId={setId}>
         <UpdateStatus id={id} setId={setId} open={open} setOpen={setOpen} />
       </CustomDialog>
     </div>
